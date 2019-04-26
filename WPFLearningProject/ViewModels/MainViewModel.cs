edae7ae51a84fcbd1b610537caba09350191f971
Akn <sibata486@gmail.com>
2019-04-26 18:20:01 +0900

    サンプルのパーツを配置　メインビューモデルにプロパティを定義

--- WPFLearningProject/ViewModels/MainViewModel.cs
+++ WPFLearningProject/ViewModels/MainViewModel.cs
@@ -5,6 +5,34 @@ using System.Text;
 using System.Threading.Tasks;
 
 namespace WPFLearningProject.ViewModels {
-    class MainViewModel {
+    class MainViewModel{
+
+        //大文字に変換して取得
+        private string _upperString;
+            
+        public string UpperString{
+            get { return this._upperString; }
+            private set {
+                if(this._upperString != value) {
+                    this._upperString = value;
+                }
+            }
+        }
+
+        //入力文字列の取得、設定
+        private string _inputString;
+
+        public string InputSring {
+            get { return this._inputString; }
+            set {
+                if(this._inputString != value) {
+                    this._inputString = value;
+
+                    this.UpperString = this._inputString.ToUpper();
+                    System.Diagnostics.Debug.WriteLine("UppertString = " + this.UpperString);
+                }
+            }
+        }
+
     }
 }
