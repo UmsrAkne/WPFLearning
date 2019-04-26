9384e599d8ef139912e0751b6549202e0dfa8f3e
Akn <sibata486@gmail.com>
2019-04-26 17:43:57 +0900

    プロジェクトのファイル配置をMVVM風に変更

--- WPFLearningProject/App.xaml.cs
+++ WPFLearningProject/App.xaml.cs
@@ -5,11 +5,28 @@ using System.Data;
 using System.Linq;
 using System.Threading.Tasks;
 using System.Windows;
+using WPFLearningProject.Views;
+using WPFLearningProject.ViewModels;
 
 namespace WPFLearningProject {
     /// <summary>
     /// App.xaml の相互作用ロジック
     /// </summary>
     public partial class App : Application {
+
+        //アプリケーション起動時に実行される OnStartupメソッドをオーバーライドする。
+        protected override void OnStartup(StartupEventArgs e) {
+            base.OnStartup(e);
+
+            var window = new MainView();
+            var viewModel = new MainViewModel();
+
+            //ウィンドウに対するViewModel　をデータコンテキストに指定？
+            //何を言っているのかよくわからない…
+            window.DataContext = viewModel;
+
+            window.Show();
+        }
+
     }
 }
